2b7a325ccbc39456f846994557f37e36c78714df
Luciano Mesquita <lucianomesquita01@gmail.com>
2020-01-04 10:41:25 -0300

    Concluindo as configurações para o Heroku

--- core/settings.py
+++ core/settings.py
@@ -1,12 +1,14 @@
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = '$po68apb#e3tcra+t^jy#^70bqrox5$lfq5-556n9stx+q@p7)'
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'estacionamento-luciano.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -48,12 +50,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+DATABASES = {'default': config('DATABASE_URL',default=default_dburl, cast=dburl), }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -81,3 +79,5 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
