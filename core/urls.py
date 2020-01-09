4b379dc1066d0ff073fe17b785c6f7d91609e301
Luciano Mesquita <lucianomesquita01@gmail.com>
2020-01-09 12:39:34 -0300

    Inserindo menu e ajustando rota principal

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('estacionamento/', include('estacionamento.urls')),
+    path('sistema/', include('estacionamento.urls')),
     path('admin/', admin.site.urls),
 ]
