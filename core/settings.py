d6b587003345f732a061bf2a04de19237a33674d
Luciano Mesquita <lucianomesquita01@gmail.com>
2020-01-21 06:26:46 -0300

    'dominio'

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'estacionamento-luciano.herokuapp.com', '192.168.0.104']
+ALLOWED_HOSTS = ['localhost', 'estacionamento-luciano.herokuapp.com', '192.168.0.104', 'lucianomesquita.tech']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
